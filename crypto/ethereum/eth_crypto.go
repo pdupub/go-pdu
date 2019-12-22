6bedb2fae566e09cc8df35243eb9fa5d8e12792b
TATAUFO <liupeng@tataufo.com>
2019-12-22 09:20:44 +0800

    refactor

--- crypto/ethereum/eth_crypto.go
+++ crypto/ethereum/eth_crypto.go
@@ -288,6 +288,7 @@ func (e EEngine) EncryptKey(priKey *crypto.PrivateKey, pass string) ([]byte, err
 	if priKey.Source != crypto.ETH {
 		return nil, crypto.ErrSourceNotMatch
 	}
+	var ekl crypto.EncryptedKeyJListV3
 	if priKey.SigType == crypto.Signature2PublicKey {
 		pk, err := parsePriKey(priKey.PriKey)
 		if err != nil {
@@ -297,9 +298,9 @@ func (e EEngine) EncryptKey(priKey *crypto.PrivateKey, pass string) ([]byte, err
 		if err != nil {
 			return nil, err
 		}
-		return json.Marshal(ekj)
+		ekl = append(ekl, ekj)
+
 	} else if priKey.SigType == crypto.MultipleSignatures {
-		var ekl crypto.EncryptedKeyJListV3
 		for _, v := range priKey.PriKey.([]interface{}) {
 			pk, err := parsePriKey(v)
 			if err != nil {
@@ -311,11 +312,8 @@ func (e EEngine) EncryptKey(priKey *crypto.PrivateKey, pass string) ([]byte, err
 			}
 			ekl = append(ekl, ekj)
 		}
-		return json.Marshal(ekl)
-
 	}
-	return nil, crypto.ErrSigTypeNotSupport
-
+	return json.Marshal(crypto.EncryptedPrivateKey{Source: crypto.ETH, SigType: priKey.SigType, EPK: ekl})
 }
 
 func (e EEngine) encryptKey(priKey *ecdsa.PrivateKey, pass string) (*crypto.EncryptedKeyJSONV3, error) {
@@ -343,22 +341,23 @@ func (e EEngine) encryptKey(priKey *ecdsa.PrivateKey, pass string) (*crypto.Encr
 // DecryptKey decrypt private key from file
 func (e EEngine) DecryptKey(keyJson []byte, pass string) (*crypto.PrivateKey, error) {
 
-	k := new(crypto.EncryptedKeyJSONV3)
-	var kl crypto.EncryptedKeyJListV3
-	if err := json.Unmarshal(keyJson, k); err == nil {
-		keyBytes, err := keystore.DecryptDataV3(k.Crypto, pass)
-		if err != nil {
-			return nil, err
-		}
-		return &crypto.PrivateKey{Source: crypto.ETH, SigType: crypto.Signature2PublicKey, PriKey: eth.ToECDSAUnsafe(keyBytes)}, nil
-	} else if err := json.Unmarshal(keyJson, &kl); err == nil {
+	var k crypto.EncryptedPrivateKey
+	if err := json.Unmarshal(keyJson, &k); err != nil {
+		return nil, err
+	} else if k.Source != crypto.ETH {
+		return nil, crypto.ErrSourceNotMatch
+	} else {
 		var priKey []*ecdsa.PrivateKey
-		for _, v := range kl {
+		for _, v := range k.EPK {
 			keyBytes, err := keystore.DecryptDataV3(v.Crypto, pass)
 			if err != nil {
 				return nil, err
 			}
-			priKey = append(priKey, eth.ToECDSAUnsafe(keyBytes))
+			pk := eth.ToECDSAUnsafe(keyBytes)
+			priKey = append(priKey, pk)
+			if k.SigType == crypto.Signature2PublicKey {
+				return &crypto.PrivateKey{Source: crypto.ETH, SigType: crypto.Signature2PublicKey, PriKey: pk}, nil
+			}
 		}
 		return &crypto.PrivateKey{Source: crypto.ETH, SigType: crypto.MultipleSignatures, PriKey: priKey}, nil
 	}
