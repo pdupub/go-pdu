65f2eccb7cfe263ffc6c61ac11e57e0d065f15f0
TATAUFO <liupeng@tataufo.com>
2020-01-05 11:45:29 +0800

    create root users

--- cmd/pdu/create.go
+++ cmd/pdu/create.go
@@ -17,9 +17,11 @@
 package main
 
 import (
+	"crypto/ecdsa"
 	"fmt"
 	"github.com/howeyc/gopass"
 	"github.com/mitchellh/go-homedir"
+	"github.com/pdupub/go-pdu/common"
 	"github.com/pdupub/go-pdu/core"
 	"github.com/pdupub/go-pdu/crypto"
 	"github.com/pdupub/go-pdu/db"
@@ -29,6 +31,7 @@ import (
 	"io/ioutil"
 	"os"
 	"path"
+	"strings"
 )
 
 // createCmd represents the create command
@@ -42,15 +45,21 @@ var createCmd = &cobra.Command{
 		}
 		fmt.Println("Database initialized successfully", dataDir)
 
-		priKey1, priKey2, err := unlockRootsKey()
+		priKeys, err := unlockRootsKeys(2)
 		if err != nil {
 			os.RemoveAll(dataDir)
 			return err
 		}
-		fmt.Println("Unlock root key", priKey1, priKey2)
-		fmt.Println("Create root users")
+		fmt.Println("Unlock root key successfully")
 
-		fmt.Println("Create universe and space-time")
+		users, err := createRootUsers(priKeys)
+		if err != nil {
+			os.RemoveAll(dataDir)
+			return err
+		}
+		fmt.Println("Create root users successfully", users[0].Gender(), users[1].Gender())
+
+		fmt.Println("Create universe and space-time successfully")
 
 		if err := udb.Close(); err != nil {
 			return err
@@ -60,13 +69,53 @@ var createCmd = &cobra.Command{
 	},
 }
 
-func unlockRootsKey() (priKey1 *crypto.PrivateKey, priKey2 *crypto.PrivateKey, err error) {
-	priKey1, err = unlockKey()
-	if err != nil {
-		return nil, nil, err
+func createRootUsers(priKeys []*crypto.PrivateKey) (users []*core.User, err error) {
+	for _, v := range priKeys {
+		for {
+
+			var pubKey crypto.PublicKey
+			if v.SigType == crypto.Signature2PublicKey {
+				pk := v.PriKey.(*ecdsa.PrivateKey)
+				pubKey = crypto.PublicKey{Source: v.Source, SigType: v.SigType, PubKey: pk.PublicKey}
+			} else if v.SigType == crypto.MultipleSignatures {
+				var pubKeys []interface{}
+				pks := v.PriKey.([]*ecdsa.PrivateKey)
+				for _, pk := range pks {
+					pubKeys = append(pubKeys, pk.PublicKey)
+				}
+				pubKey = crypto.PublicKey{Source: v.Source, SigType: v.SigType, PubKey: pubKeys}
+			} else {
+				return users, crypto.ErrSigTypeNotSupport
+			}
+
+			var rootName, rootExtra, isSave string
+			fmt.Print("name: ")
+			fmt.Scan(&rootName)
+			fmt.Print("extra: ")
+			fmt.Scan(&rootExtra)
+			user := core.CreateRootUser(pubKey, rootName, rootExtra)
+			fmt.Println("ID", common.Hash2String(user.ID()), "name", user.Name, "extra", user.DOBExtra, "gender", user.Gender())
+			fmt.Print("save new user (yes/no): ")
+			fmt.Scan(&isSave)
+			if strings.ToUpper(isSave) == "YES" || strings.ToUpper(isSave) == "Y" {
+				users = append(users, user)
+				break
+			}
+		}
+
 	}
-	priKey2, err = unlockKey()
-	return priKey1, priKey2, err
+	return users, err
+}
+
+func unlockRootsKeys(cnt int) (priKeys []*crypto.PrivateKey, err error) {
+	for i := 0; i < cnt; i++ {
+		priKey, err := unlockKey()
+		if err != nil {
+			return priKeys, err
+		}
+		priKeys = append(priKeys, priKey)
+	}
+	return priKeys, err
 }
 
 func unlockKey() (*crypto.PrivateKey, error) {
