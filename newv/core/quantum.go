3bc854a280f61264c59d1fcf2b75df0c7b87c037
Liu Peng <liupeng@tataufo.com>
2022-01-28 18:59:17 +0800

    add more comments

--- newv/core/quantum.go
+++ newv/core/quantum.go
@@ -40,11 +40,27 @@ var (
 )
 
 const (
-	// QuantumTypeInfo specifies
+	// QuantumTypeInfo specifies quantum which user just want to share information.
 	QuantumTypeInfo = 1
-	// QuantumTypeProfile specifies
+
+	// QuantumTypeProfile specifies the quantum to update user's (signer's) profile.
+	// contents = [key1(QCFmtStringTEXT), value1, key2, value2 ...]
+	// if user want to update key1, send new QuantumTypeProfile quantum with
+	// contents = [key1, newValue ...]
+	// if user want to delete key1, send new QuantumTypeProfile quantum with
+	// contents = [key1, newValue which content with empty data]
 	QuantumTypeProfile = 2
-	// QuantumTypeRule specifies
+
+	// QuantumTypeRule specifies the quantum of rule to build new group
+	// contents[0] is base group of current group
+	// {fmt:QCFmtBytesSignature, data:signature of base group rule quantum}
+	// contents[1] is the number of co-signature from users in current group
+	// {fmt: QCFmtStringInt, data:1} number should at least 1
+	// contents[2] is the max number of create by one user
+	// {fmt: QCFmtStringInt, data:1} -1 means no limit, 0 means not allowed
+	// contents[3] ~ contents[15] is the initial users in this group
+	// {fmt: QCFmtBytesAddress, data:0x1232...}
+	// signer of this group is also the initial user in this group
 	QuantumTypeRule = 3
 	// QuantumTypeInvite specifies
 	QuantumTypeInvite = 4
@@ -67,9 +83,11 @@ type Quantum struct {
 }
 
 const (
-	QCFmtStringTEXT = 1
-	QCFmtStringURL  = 2
-	QCFmtStringJSON = 3
+	QCFmtStringTEXT  = 1
+	QCFmtStringURL   = 2
+	QCFmtStringJSON  = 3
+	QCFmtStringInt   = 4
+	QCFmtStringFloat = 5
 
 	QCFmtBytesAddress   = 33
 	QCFmtBytesSignature = 34
@@ -87,7 +105,7 @@ const (
 // QContent is one piece of data in Quantum,
 // all variables should be in alphabetical order.
 type QContent struct {
-	Data   []byte `json:"data"`
+	Data   []byte `json:"data,omitempty"`
 	Format int    `json:"fmt"`
 }
 
