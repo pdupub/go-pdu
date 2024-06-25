12913fa9945db2a5080803f91faab7e168b14724
Peng Liu <pdupub@gmail.com>
2024-06-25 10:25:01 +0800

    deal quantum from metamask sign

--- cmd/pdu/main.go
+++ cmd/pdu/main.go
@@ -21,7 +21,6 @@ import (
 	"os"
 
 	"github.com/pdupub/go-pdu/node"
-	"github.com/pdupub/go-pdu/udb"
 	"github.com/spf13/cobra"
 )
 
@@ -32,6 +31,10 @@ func main() {
 		Run: func(cmd *cobra.Command, args []string) {
 			fmt.Println("P2P node running")
 			// 初始化并启动节点的代码
+			listenPort := 4001 // P2P 监听端口
+			webPort := 8546    // Web 服务器端口
+			rpcPort := 8545    // RPC 服务器端口
+			node.Run(listenPort, webPort, rpcPort)
 		},
 	}
 
@@ -44,38 +47,7 @@ func main() {
 		},
 	}
 
-	var nodeCmd = &cobra.Command{
-		Use:   "node",
-		Short: "Run node command",
-		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("P2P node running")
-			listenPort := 4001 // P2P 监听端口
-			webPort := 8546    // Web 服务器端口
-			rpcPort := 8545    // RPC 服务器端口
-			node.Run(listenPort, webPort, rpcPort)
-		},
-	}
-
-	var DBCmd = &cobra.Command{
-		Use:   "db",
-		Short: "Run db command",
-		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("Running db...")
-			dbName := "udb.db"
-			db, err := udb.InitDB(dbName)
-			defer db.CloseDB()
-
-			if err != nil {
-				fmt.Println(err)
-				os.Exit(1)
-			}
-			// db方法的代码
-		},
-	}
-
 	rootCmd.AddCommand(testCmd)
-	rootCmd.AddCommand(nodeCmd)
-	rootCmd.AddCommand(DBCmd)
 
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
